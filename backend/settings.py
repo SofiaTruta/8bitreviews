8064449a7b6a3ad2273453fdd8106b465efdb6ce
sofiatruta <sofiatruta@gmail.com>
2023-11-22 11:58:28 +0000

    cors allowed origins

--- backend/settings.py
+++ backend/settings.py
@@ -139,5 +139,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS =[
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "8bitreviews-frontend-production.up.railway.app"
 ]
