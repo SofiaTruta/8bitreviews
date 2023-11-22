5eb2e8cc4f6a1cb125bc359d801aa24cec56c46c
sofiatruta <sofiatruta@gmail.com>
2023-11-22 12:08:08 +0000

    adding internal cors as link just in case

--- backend/settings.py
+++ backend/settings.py
@@ -140,5 +140,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS =[
     "http://localhost:3000",
-    "8bitreviews-frontend-production.up.railway.app"
+    "8bitreviews-frontend-production.up.railway.app",
+    "8bitreviews-frontend.railway.internal"
 ]
